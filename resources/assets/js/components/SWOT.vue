e3c47adba921ae3d192f2750256aaafde4c25d67
Archit Wankhede <architwankhede@gmail.com>
2018-01-24 00:35:38 +0530

    Added default toast message

--- resources/assets/js/components/SWOT.vue
+++ resources/assets/js/components/SWOT.vue
@@ -5,7 +5,7 @@
                 currentSWOT: {},
                 SWOTs: {},
                 showBody: false,
-                toastMessage: ''
+                toastMessage: 'Hello SWOT'
             }
         },
 
